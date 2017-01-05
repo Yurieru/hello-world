5b12895363e33ea03b2c450d22dc59a57cf49624
HP <HP@DANIEL>
2017-01-05 10:48:52 -0700

    Add project files.

---
+++ Binarios_recursivos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binarios_recursivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Dec;
+            string bin;
+            Console.WriteLine("Introduce un numero decimal entero");
+            Dec = Convert.ToInt32(Console.ReadLine());
+            bin = tip(Dec);
+            Console.WriteLine("el numero en binario es:  " + bin);
+            Console.ReadKey();
+        }
+
+        public static string tip(int n)
+        {
+            string respuesta=(n%2).ToString();
+            if (n < 2)
+            {
+                respuesta = (n % 2).ToString();
+            }
+            else
+            {
+                respuesta =proceso(n / 2)+respuesta;
+            }
+            return respuesta;
+        }
+
+        public static int proceso(int n)
+        {
+            int respuesta = n % 2;
+            if (n < 2)
+            {
+                respuesta = n % 2;
+            }
+            else
+            {
+                respuesta += 10 * proceso(n / 2);
+            }
+            return respuesta;
+        }
+
+    }
+}
